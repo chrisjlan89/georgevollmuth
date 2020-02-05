440fb231cf81138bc42d0b2b3c3aba48bbfd43b9
Christoph Lantier <clantier@momentumsolar.com>
2020-02-05 17:56:23 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./assets/kiY0cf8W_400x400.jpeg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>George Vollmuth !!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
